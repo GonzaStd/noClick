d3e413ef9a4acca0b411dba5c963bdc3f732bcb2
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-08-23 22:14:08 -0300

    Removed: Main.java because it is no longer needed. The new "Main" class is "NoClickGame". Changed: "Main" to "NoClickGame"

--- lwjgl3/src/main/java/io/gonzastd/noclick/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/gonzastd/noclick/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package io.gonzastd.noclick.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.gonzastd.noclick.Main;
+import io.gonzastd.noclick.noclick.NoClickGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+        return new Lwjgl3Application(new NoClickGame(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
