a5b42b17eaf17fc03b86094395f11d62c7560eec
Gonzalo Vattino <gonzalo.vattino.secu@gmail.com>
2026-08-28 11:31:44 -0300

    Added: Map and car layer initialization with facing attribute.

--- core/src/main/java/io/gonzastd/noclick/noclick/GameScreen.java
+++ core/src/main/java/io/gonzastd/noclick/noclick/GameScreen.java
@@ -1,12 +1,26 @@
 package io.gonzastd.noclick.noclick;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GameScreen implements Screen {
 
     @Override
     public void show() {
+        TiledMap map = new TmxMapLoader().load("map/level-one.tmx");
+        MapLayer carLayer = map.getLayers().get("cars");
 
+        for (MapObject object : carLayer.getObjects()) {
+            if (object instanceof RectangleMapObject) {
+                Rectangle rect = ((RectangleMapObject) object).getRectangle();
+                String plate = object.getProperties().get("facing")
+            }
+        }
     }
 
     @Override
