84327851d32814cc563b26caa0f4ba7dd45e412d
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-09-04 10:10:41 -0300

    Changed: From interface to abstract (father) class. Every basic drawable has position and bounds. Removed "dispose" implementation to avoid unnecessary coupling.

--- core/src/main/java/io/gonzastd/noclick/objects/BasicDrawable.java
+++ core/src/main/java/io/gonzastd/noclick/objects/BasicDrawable.java
@@ -1,8 +1,35 @@
 package io.gonzastd.noclick.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
-public interface BasicDrawable extends Disposable {
-    void draw(SpriteBatch batch);
+public abstract class BasicDrawable {
+
+    protected float spriteWidth;
+    protected float spriteHeight;
+    protected Rectangle bounds;
+    protected Vector2 position;
+
+    public BasicDrawable(float startX, float startY, float width, float height) {
+        this.spriteWidth = width;
+        this.spriteHeight = height;
+        this.position = new Vector2(startX, startY);
+    }
+
+
+    public void updateBounds() {
+        bounds.setPosition(position.x, position.y);
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public abstract void draw(SpriteBatch batch);
+    public abstract void dispose();
 }
