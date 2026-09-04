d009b2a0e46a4c8d205ef967e2025dd9e952f0e2
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-09-04 11:04:44 -0300

    Changed: width and height to final.

--- core/src/main/java/io/gonzastd/noclick/objects/BasicDrawable.java
+++ core/src/main/java/io/gonzastd/noclick/objects/BasicDrawable.java
@@ -11,9 +11,9 @@ public abstract class BasicDrawable {
     protected Rectangle bounds;
     protected Vector2 position;
 
-    public BasicDrawable(float startX, float startY, float width, float height) {
-        this.SPRITE_WIDTH = width;
-        this.SPRITE_HEIGHT = height;
+    public BasicDrawable(float startX, float startY, final float SPRITE_WIDTH, final float SPRITE_HEIGHT) {
+        this.SPRITE_WIDTH = SPRITE_WIDTH;
+        this.SPRITE_HEIGHT = SPRITE_HEIGHT;
         this.position = new Vector2(startX, startY);
         this.bounds = new Rectangle(
             this.position.x,
